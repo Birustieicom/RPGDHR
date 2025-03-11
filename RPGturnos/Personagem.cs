f1944a6a84e964c78c21b27c2c8f6c78fe2f9000
Birustieicom <birustieicomsecundaria@gmail.com>
2025-03-11 11:45:46 -0300

    Jogo de RPG

---
+++ RPGturnos/Personagem.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPGturnos
+{
+    public class Personagem
+    {
+        string nome;
+        string classe;
+        int pontosdevida;
+        int pontosdeenergia;
+        int ataque;
+        int resistencia;
+        int experiencia;
+        int nivel;
+        int vidapornivel;
+        int energiapornivel;
+
+        public void criacaoPersonagem()
+        {
+            Console.WriteLine("Insira o nome do seu personagem");
+            nome = Console.ReadLine();
+            Console.Clear();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Classes disponíveis");
+                Console.WriteLine("");
+                Console.WriteLine("1 - Guerreiro");
+                Console.WriteLine("2 - Arqueiro");
+                Console.WriteLine("3 - Mago");
+                Console.WriteLine("4 - Monge");
+                Console.WriteLine("");
+                Console.WriteLine("Escreva qual a classe do seu personagem");
+                classe = Console.ReadLine();
+            } while (classe != "Guerreiro" && classe != "Arqueiro" && classe != "Mago" && classe != "Monge");
+
+            Console.Clear();
+            Console.WriteLine("Bem vindo " + nome);
+            Console.WriteLine("Você pertence a classe " + classe);
+            Console.WriteLine("Seus atributos são:");
+            Console.WriteLine("");
+
+            if (classe == "Guerreiro")
+            {
+                construirGuerreiro();
+                Console.ReadKey();
+            }
+            else if (classe == "Arqueiro")
+            {
+                construirArqueiro();
+                Console.ReadKey();
+            }
+            else if (classe == "Mago")
+            {
+                construirMago();
+                Console.ReadKey();
+            }
+            else
+            {
+                construirMonge();
+                Console.ReadKey();
+            }
+        }
+
+        public void construirGuerreiro()
+        {
+            experiencia = 0;
+            nivel = 1;
+            pontosdevida = 200;
+            pontosdeenergia = 50;
+            ataque = 10;
+            resistencia = 10;
+            vidapornivel = 25;
+            energiapornivel = 5;
+
+            Console.WriteLine("Nível: " + nivel);
+            Console.WriteLine("Experiência: " + experiencia);
+            Console.WriteLine("Pontos de Vida: " + pontosdevida);
+            Console.WriteLine("Pontos de Energia: " + pontosdeenergia);
+            Console.WriteLine("Ataque: " + ataque);
+            Console.WriteLine("Resistência: " + resistencia);
+            Console.WriteLine("Vida recebida por Nível: " + vidapornivel);
+            Console.WriteLine("Energia recebida por Nível: " + energiapornivel);
+        }
+
+        public void construirArqueiro()
+        {
+            experiencia = 0;
+            nivel = 1;
+            pontosdevida = 125;
+            pontosdeenergia = 125;
+            ataque = 14;
+            resistencia = 6;
+            vidapornivel = 15;
+            energiapornivel = 15;
+
+            Console.WriteLine("Nível: " + nivel);
+            Console.WriteLine("Experiência: " + experiencia);
+            Console.WriteLine("Pontos de Vida: " + pontosdevida);
+            Console.WriteLine("Pontos de Energia: " + pontosdeenergia);
+            Console.WriteLine("Ataque: " + ataque);
+            Console.WriteLine("Resistência: " + resistencia);
+            Console.WriteLine("Vida recebida por Nível: " + vidapornivel);
+            Console.WriteLine("Energia recebida por Nível: " + energiapornivel);
+        }
+
+        public void construirMago()
+        {
+            experiencia = 0;
+            nivel = 1;
+            pontosdevida = 75;
+            pontosdeenergia = 175;
+            ataque = 16;
+            resistencia = 4;
+            vidapornivel = 5;
+            energiapornivel = 25;
+
+            Console.WriteLine("Nível: " + nivel);
+            Console.WriteLine("Experiência: " + experiencia);
+            Console.WriteLine("Pontos de Vida: " + pontosdevida);
+            Console.WriteLine("Pontos de Energia: " + pontosdeenergia);
+            Console.WriteLine("Ataque: " + ataque);
+            Console.WriteLine("Resistência: " + resistencia);
+            Console.WriteLine("Vida recebida por Nível: " + vidapornivel);
+            Console.WriteLine("Energia recebida por Nível: " + energiapornivel);
+        }
+
+        public void construirMonge()
+        {
+            experiencia = 0;
+            nivel = 1;
+            pontosdevida = 150;
+            pontosdeenergia = 100;
+            ataque = 8;
+            resistencia = 12;
+            vidapornivel = 20;
+            energiapornivel = 10;
+
+            Console.WriteLine("Nível: " + nivel);
+            Console.WriteLine("Experiência: " + experiencia);
+            Console.WriteLine("Pontos de Vida: " + pontosdevida);
+            Console.WriteLine("Pontos de Energia: " + pontosdeenergia);
+            Console.WriteLine("Ataque: " + ataque);
+            Console.WriteLine("Resistência: " + resistencia);
+            Console.WriteLine("Vida recebida por Nível: " + vidapornivel);
+            Console.WriteLine("Energia recebida por Nível: " + energiapornivel);
+        }
+    }
+}
+
---
+++ RPGturnos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RPGturnos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menu;
+            Console.WriteLine("RPG LEGAL: O JOGO");
+            Console.WriteLine("Pressione qualquer tecla para começar");
+            Console.ReadKey();
+            Console.WriteLine("");
+
+            do
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1 - Novo Jogo");
+                Console.WriteLine("2 - Sair");
+                menu = Console.ReadLine();
+                Console.Clear();
+                if (menu == "1")
+                {
+                    Personagem Char = new Personagem();
+                    Char.criacaoPersonagem();
+                }
+                else if (menu == "2")
+                {
+
+                }
+                else if (menu != "1" && menu != "2")
+                {
+                    Console.WriteLine("Esta opção não está disponível");
+                    Console.WriteLine("Tente outra opção");
+                    Console.WriteLine("");
+                }
+            } while (menu != "1" && menu != "2");
+ 
+        }
+    }
+}
