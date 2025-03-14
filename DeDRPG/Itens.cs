1c937df46222e77dcd4085153f84f0499a303a58
Birustieicom <birustieicomsecundaria@gmail.com>
2025-03-14 12:30:31 -0300

    atualização

---
+++ DeDRPG/Itens.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPGturnos
+{
+    class Itens
+    {
+        string[,] equipamentos = new string[20, 5];
+
+    }
+}
